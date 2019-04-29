597e307970e26e7dc2a5d11cb28a0ef1ca2452f0
Ivan <beatkeen@gmail.com>
2019-04-29 13:33:11 +0300

    [firefox] Add user.js

---
+++ firefox/.mozilla/firefox/main/user.js
@@ -0,0 +1,11 @@
+// Don't show a dialog before entering Caret Browsing mode.
+user_pref("accessibility.warn_on_browsewithcaret", false);
+
+// Don't close the window when closing last tab.
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+
+// Disable Pocket.
+user_pref("extensions.pocket.enabled", false);
+
+// Disable about:config warning.
+user_pref("general.warnOnAboutConfig", false);
