05c3fb213335863c69c460a520c5f08532954812
Ivan <beatkeen@gmail.com>
2019-04-30 21:20:17 +0300

    [scripts] Add tmuxdd.sh

---
+++ scripts/tmuxdd.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Tmux dropdown launch script.
+# Takes dropdown window name as an argument.
+
+[ -z "$1" ] && exit
+
+dd_name=$1
+
+# Check if tmux dropdown is already launched.
+ps aux | grep "urxvt -name $dd_name" | grep -v "grep"
+if [ "$?" != 0 ]; then
+    session_name="dropdown"
+    tmux_cmd="tmux new-session -A -s $session_name"
+    i3 "exec --no-startup-id urxvt -name $dd_name -e $tmux_cmd"
+
+    # Wait until the session is ready.
+    while ! tmux ls | grep $session_name | grep attached
+    do
+        continue
+    done
+fi
+
+# Show dropdown window.
+i3 "[instance=\"$dd_name\"] scratchpad show; [instance=\"$dd_name\"] move position center"
