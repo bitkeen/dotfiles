3d3cb6b51e5ead0a55087ea32cfbc69c2b447170
Ivan <beatkeen@gmail.com>
2019-05-01 17:24:47 +0300

    [scripts] Add a script to expand aliases inside ranger's :shell command

---
+++ scripts/ranger-shell.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# From https://github.com/ranger/ranger/wiki/Shell-aliases.
+
+# EXPL: simplify when ranger-shell.sh -c $SHELL
+if [[ $# == 2 && $1 == -c && ($2 == $SHELL || $2 == '$SHELL') ]]
+then shift 2; fi
+
+# EXPL: overwrite when SHELL=ranger-shell.sh
+[[ -x /usr/bin/zsh ]] \
+&& export SHELL=/usr/bin/zsh \
+|| export SHELL=/usr/bin/bash
+
+# USE: Interactive shell
+(($#)) || exec ${SHELL:?}
+
+# USE: Cmdline populated with aliases
+[[ $1 != -c ]] || shift
+exec ${SHELL:?} -c '
+(( $(id -u) )) || HOME=/home/${SUDO_USER:-${USER:-${USERNAME:?}}}
+source ~/.bash_aliases
+source ~/.bash_aliases.local
+[[ ${SHELL:?} =~ zsh$ ]] && setopt aliases || shopt -s expand_aliases
+eval "$@"
+' "$0" "$@"
