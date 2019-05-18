80e35ed17c17ea41481f84cdc1a439e9c2ba6f31
Ivan <beatkeen@gmail.com>
2019-05-18 15:00:03 +0300

    [scripts] Use POSIX shell instead of bash in renumber-sessions.sh

--- scripts/renumber-sessions.sh
+++ scripts/renumber-sessions.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # From https://github.com/maximbaz/dotfiles/commit/925a5b88a8263805a5a24c6198dad23bfa62f44d.
 
 sessions=$(tmux ls | grep '^[0-9]\+:' | cut -f1 -d':' | sort)
@@ -6,6 +6,6 @@ sessions=$(tmux ls | grep '^[0-9]\+:' | cut -f1 -d':' | sort)
 new=0
 for old in $sessions
 do
-  tmux rename -t $old $new
-  ((new++))
+  tmux rename -t "$old" "$new"
+  new=$(( new+1 ))
 done
