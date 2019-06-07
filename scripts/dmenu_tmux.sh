462713c87a988fe0bae7bfd884adf6c92975cc03
Ivan <beatkeen@gmail.com>
2019-06-07 23:37:58 +0300

    [scripts] Add a promt to the dmenu_tmux script

--- scripts/dmenu_tmux.sh
+++ scripts/dmenu_tmux.sh
@@ -4,7 +4,7 @@
 
 sessions="$(tmux ls | awk -F ':' '{print $1}')"
 
-choice=$(echo "$sessions" | dmenu -i "$@")
+choice=$(echo "$sessions" | dmenu -p 'tmux attach -t ' -i "$@")
 
 if [ -n "$choice" ]; then
     i3 "exec --no-startup-id urxvt -e bash -i -c 'tmux attach -t $choice'"
