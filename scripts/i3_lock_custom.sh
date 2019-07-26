c718e350ac89a5c05c05ad0035c9cd1e2a153f91
Ivan <beatkeen@gmail.com>
2019-07-26 10:19:20 +0300

    [scripts] Use xsecurelock, refactor the locking script

--- scripts/i3_lock_custom.sh
+++ scripts/i3_lock_custom.sh
@@ -1,14 +1,13 @@
-#!/usr/bin/env bash
-if [ "$(command -v xkb-switch)" ]; then
-    # Switch keyboard layout to US so that it's easier to enter
-    # the password to unlock the screen.
-    xkb-switch -s us
-fi
+#!/bin/sh
+# Run a screen locking program, but first stop audio playback,
+# and switch the keyboard layout to English.
 
-if [ "$(command -v mpc)" ]; then
-    # Pause mpd through mpc if it exists.
-    mpc pause
-fi
+# Switch keyboard layout to US so that it's easier to enter
+# the password to unlock the screen.
+[ "$(command -v xkb-switch)" ] && xkb-switch -s us
 
-# Lock screen.
-i3lock -u -i "$HOME/Pictures/system/lock-screen.png"
+# Pause mpd through mpc if it exists.
+[ "$(command -v mpc)" ] && mpc pause
+
+# Lock the screen.
+xsecurelock
