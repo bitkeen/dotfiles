6202b85647b74544bd048f69436249ed4eebf71f
Ivan <beatkeen@gmail.com>
2019-08-05 12:11:17 +0300

    [ipython] Import math and time at startup

--- ipython/.ipython/profile_default/startup/10-imports.py
+++ ipython/.ipython/profile_default/startup/10-imports.py
@@ -1,8 +1,12 @@
+import math
 import pprint
 import re
+import time
 from datetime import datetime, timedelta
 
 print('')
+print('>>> import math')
 print('>>> import pprint')
 print('>>> import re')
+print('>>> import time')
 print('>>> from datetime import datetime, timedelta')
