cef91a07876719739d4d8f0ac4ca0fa3ee972952
Ivan <beatkeen@gmail.com>
2020-02-27 19:46:43 +0000

    [scripts] Make i3_tmuxdd accept session name as an argument

--- scripts/.local/bin/i3_tmuxdd.sh
+++ scripts/.local/bin/i3_tmuxdd.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 # Tmux dropdown launch script.
-# Takes dropdown window name as an argument.
+#
+# Arguments:
+# $1 - terminal window name
+# $2 - tmux session to attach to
 
-[ -z "$1" ] && exit
+[ -z "$1" ] && exit 1
+[ -z "$2" ] && exit 1
 
 dd_name=$1
+session_name=$2
 
 # Check if tmux dropdown is already launched.
 if ! pgrep -f "urxvt -name $dd_name"; then
-    session_name="dropdown"
     tmux_cmd="tmux new-session -A -s $session_name"
     i3 "exec --no-startup-id urxvt -name $dd_name -e $tmux_cmd"
 
