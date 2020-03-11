b01caee07ce90ac90a40c606cd4f5f60c8293ae3
Ivan <beatkeen@gmail.com>
2020-03-11 06:03:25 +0000

    [ipython] Import os and sys

--- ipython/.ipython/profile_default/startup/10-imports.py
+++ ipython/.ipython/profile_default/startup/10-imports.py
@@ -1,12 +1,16 @@
 import math
+import os
 import pprint
 import re
+import sys
 import time
 from datetime import datetime, timedelta
 
 print('')
 print('>>> import math')
+print('>>> import os')
 print('>>> import pprint')
 print('>>> import re')
+print('>>> import sys')
 print('>>> import time')
 print('>>> from datetime import datetime, timedelta')
