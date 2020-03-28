14c4d0b33dddbcb592675b0ca41fcd6eea11f8c8
Ivan <beatkeen@gmail.com>
2020-03-28 20:24:39 +0000

    [scripts] Allow to "switch" sessions in i3_tmuxdd.sh

--- scripts/.local/bin/i3_tmuxdd.sh
+++ scripts/.local/bin/i3_tmuxdd.sh
@@ -8,19 +8,35 @@
 [ -z "$1" ] && exit 1
 [ -z "$2" ] && exit 1
 
-dd_name=$1
-session_name=$2
+dd_name="$1"
+session_name="$2"
 
-# Check if tmux dropdown is already launched.
-if ! pgrep -f "urxvt -name $dd_name"; then
+launch_new_terminal() {
+    # Tmux dropdown is not running.
     tmux_cmd="tmux new-session -A -s $session_name"
     i3 "exec --no-startup-id urxvt -name $dd_name -e $tmux_cmd"
 
     # Wait until the session is ready.
-    while ! tmux ls | grep $session_name | grep attached
+    while ! tmux ls | grep "$session_name" | grep attached
     do
         continue
     done
+}
+
+existing_dd="$(pgrep -fa "urxvt -name $dd_name")"
+if [ -n "$existing_dd" ]; then
+    # Tmux dropdown is already running.
+    attached_session="$(echo "$existing_dd" | awk '{ print $NF }')"
+    if [ "$attached_session" != "$session_name" ]; then
+        # Attached session is different from the one passed in
+        # arguments. Kill the terminal and launch a new one.
+        existing_dd_pid="$(echo "$existing_dd" | awk '{ print $1 }')"
+        kill "$existing_dd_pid"
+        launch_new_terminal
+    fi
+else
+    # Tmux dropdown is not running.
+    launch_new_terminal
 fi
 
 # Show dropdown window.
