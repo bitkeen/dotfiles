68a4b53e16f1a79d037483073ae12a8dbdea5dc8
Ivan <beatkeen@gmail.com>
2020-06-03 08:27:33 +0000

    [ipython] Add c-a and c-e bindings to vi mode

--- ipython/.ipython/profile_default/startup/00-bindings.py
+++ ipython/.ipython/profile_default/startup/00-bindings.py
@@ -3,20 +3,35 @@
 from IPython import get_ipython
 from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.keys import Keys
+from prompt_toolkit.key_binding.bindings.named_commands import get_by_name
 from prompt_toolkit.filters import HasFocus, HasSelection
 
 ip = get_ipython()
 
-def clear_screen(event):
-    event.cli.renderer.clear()
-
 # Register the shortcut if IPython is using prompt_toolkit.
 if getattr(ip, 'pt_app', None):
     registry = ip.pt_app.key_bindings
-    registry.add_binding(
+    registry.add(
         Keys.ControlF,
         filter=(
             HasFocus(DEFAULT_BUFFER)
             & ~HasSelection()
         )
-    )(clear_screen)
+    )(get_by_name("clear-screen"))
+
+    # Add c-a and c-e to vi mode.
+    registry.add(
+        Keys.ControlA,
+        filter=(
+            HasFocus(DEFAULT_BUFFER)
+            & ~HasSelection()
+        )
+    )(get_by_name("beginning-of-line"))
+
+    registry.add(
+        Keys.ControlE,
+        filter=(
+            HasFocus(DEFAULT_BUFFER)
+            & ~HasSelection()
+        )
+    )(get_by_name("end-of-line"))
