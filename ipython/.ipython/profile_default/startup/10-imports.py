06a9b9b9f22dd13cf66698fdf0a3da07d67ba7c7
Ivan <beatkeen@gmail.com>
2020-07-06 13:58:19 +0000

    [ipython] Improve imports

--- ipython/.ipython/profile_default/startup/10-imports.py
+++ ipython/.ipython/profile_default/startup/10-imports.py
@@ -1,18 +1,15 @@
-import math
-import os
-import pprint
-import re
-import sys
-import time
-import urllib.parse as up
-from datetime import datetime, timedelta
+imports = [
+    'import math',
+    'import os',
+    'import pprint',
+    'import random',
+    'import re',
+    'import sys',
+    'import time',
+    'import urllib.parse as up',
+    'from datetime import datetime, timedelta',
+]
 
-print('')
-print('>>> import math')
-print('>>> import os')
-print('>>> import pprint')
-print('>>> import re')
-print('>>> import sys')
-print('>>> import time')
-print('>>> import urllib.parse as up')
-print('>>> from datetime import datetime, timedelta')
+for i in imports:
+    exec(i)
+    print(f'>>> {i}')
