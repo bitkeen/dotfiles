61df1c7421ab1ab0743df6f01faf3805bf3f522e
Ivan <beatkeen@gmail.com>
2022-04-23 16:24:31 +0000

    [ipython] Configure timeoutlen

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -293,6 +293,11 @@ c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
 c.TerminalInteractiveShell.editing_mode = 'vi'
 
+## The time in milliseconds that is waited for a mapped key
+#         sequence to complete.
+#  Default: 0.5
+c.TerminalInteractiveShell.timeoutlen = 0.15
+
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
 #c.TerminalInteractiveShell.editor = 'vi'
 
