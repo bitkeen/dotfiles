dc67c573011bcc7b3214d319b57b40914545dedd
Ivan <beatkeen@gmail.com>
2022-07-20 09:26:48 +0000

    [ipython] Clean up & fix bindings script

--- ipython/.ipython/profile_default/startup/00-bindings.py
+++ ipython/.ipython/profile_default/startup/00-bindings.py
@@ -2,8 +2,15 @@
 import sqlite3
 from pathlib import Path
 
-import plumbum
-import pyfzf
+try:
+    import plumbum
+except ModuleNotFoundError as e:
+    print(e)
+try:
+    import pyfzf
+except ModuleNotFoundError as e:
+    print(e)
+
 from IPython import get_ipython
 from prompt_toolkit.application.current import get_app
 from prompt_toolkit.enums import DEFAULT_BUFFER
@@ -12,20 +19,10 @@ from prompt_toolkit.filters.base import Condition
 from prompt_toolkit.key_binding.bindings.named_commands import get_by_name
 from prompt_toolkit.keys import Keys
 
-
-@Condition
-def has_suggestion() -> bool:
-    """
-    Enable when the current buffer has a suggestion.
-    """
-    buffer = get_app().current_buffer
-    return buffer.suggestion is not None and buffer.suggestion.text != ''
-
-
 FZF_OPTIONS = ('--read0'
                ' --preview "echo {} | bat --plain --color always -l python"'
                ' --preview-window="nohidden"')
-HIST_DB_FILE = f'{str(Path.home())}/.ipython/profile_default/history.sqlite'
+HIST_DB_FILE = f'{(Path.home())}/.ipython/profile_default/history.sqlite'
 HIST_SELECT_QUERY = """
 SELECT DISTINCT
     source_raw
@@ -36,7 +33,13 @@ ORDER BY
     line DESC
 """
 
-ip = get_ipython()
+
+@Condition
+def has_suggestion() -> bool:
+    """Enable when the current buffer has a suggestion."""
+    buffer = get_app().current_buffer
+    return buffer.suggestion is not None and buffer.suggestion.text != ''
+
 
 def fzf_history(event):
     """Get item from history with fzf.
@@ -58,38 +61,42 @@ def fzf_history(event):
     except (IndexError, plumbum.commands.processes.ProcessExecutionError):
         pass
 
-# Register the shortcuts if IPython is using prompt_toolkit.
-if getattr(ip, 'pt_app', None):
-    registry = ip.pt_app.key_bindings
 
-    registry.add(
-        Keys.ControlF,
-        filter=(
-            has_focus(DEFAULT_BUFFER)
-            & ~has_selection
-        )
-    )(get_by_name('clear-screen'))
+if 'plumbum' in locals() and 'pyfzf' in locals():
+    ip = get_ipython()
 
-    # Add c-a and c-e to vi mode.
-    registry.add(
-        Keys.ControlA,
-        filter=(
-            has_focus(DEFAULT_BUFFER)
-            & ~has_selection
-        )
-    )(get_by_name('beginning-of-line'))
+    # Register the shortcuts if IPython is using prompt_toolkit.
+    if getattr(ip, 'pt_app', None):
+        registry = ip.pt_app.key_bindings
 
-    registry.add(
-        Keys.ControlE,
-        filter=(
-            has_focus(DEFAULT_BUFFER)
-            & ~has_selection
-            & ~has_suggestion
-        )
-    )(get_by_name('end-of-line'))
+        registry.add(
+            Keys.ControlF,
+            filter=(
+                has_focus(DEFAULT_BUFFER)
+                & ~has_selection
+            )
+        )(get_by_name('clear-screen'))
 
-    # Use fzf for history.
-    registry.add(
-        Keys.ControlR,
-        filter=has_focus(DEFAULT_BUFFER)
-    )(fzf_history)
+        # Add c-a and c-e to vi mode.
+        registry.add(
+            Keys.ControlA,
+            filter=(
+                has_focus(DEFAULT_BUFFER)
+                & ~has_selection
+            )
+        )(get_by_name('beginning-of-line'))
+
+        registry.add(
+            Keys.ControlE,
+            filter=(
+                has_focus(DEFAULT_BUFFER)
+                & ~has_selection
+                & ~has_suggestion
+            )
+        )(get_by_name('end-of-line'))
+
+        # Use fzf for history.
+        registry.add(
+            Keys.ControlR,
+            filter=has_focus(DEFAULT_BUFFER)
+        )(fzf_history)
