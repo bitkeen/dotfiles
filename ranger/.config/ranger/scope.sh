25753628212692062feb9352765297fbd62bccf0
Ivan <beatkeen@gmail.com>
2022-10-04 07:45:05 +0000

    [ranger] Fix previews for image/avif

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -108,7 +108,8 @@ handle_image() {
             orientation="$( identify -format '%[EXIF:Orientation]\n' -- "${FILE_PATH}" )"
             # If orientation data is present and the image actually
             # needs rotating ("1" means no rotation)...
-            if [[ -n "$orientation" && "$orientation" != 1 ]]; then
+            if { [ -n "${orientation}" ] && [ "${orientation}" != 1 ]; } \
+                    || [ "${mimetype}" = 'image/avif' ]; then
                 # ...auto-rotate the image according to the EXIF data.
                 convert -- "${FILE_PATH}" -auto-orient "${IMAGE_CACHE_PATH}" && exit 6
             fi
